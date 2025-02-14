979e247da84c0313fb42f34c03bed6a197804021
harshps900 <151460810+harshps900@users.noreply.github.com>
2025-02-14 12:38:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-//your JS code here. If required.
+ let colorSelect = document.getElementById("colorSelect");
+      let removeBtn = document.getElementById("removeBtn");
+
+      removeBtn.addEventListener("click", () => {
+        let selectedOption = colorSelect.value;
+        for (let i = 0; i < colorSelect.options.length; i++) {
+          if (colorSelect.options[i].value === selectedOption) {
+            colorSelect.remove(i);
+            break; 
+          }
+        }
+      });
