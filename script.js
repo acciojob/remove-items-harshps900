aa6004c881aa1599bc8c5f9b6a20a3f2c340bb04
harshps900 <151460810+harshps900@users.noreply.github.com>
2025-02-14 12:41:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,9 @@
  let colorSelect = document.getElementById("colorSelect");
       let removeBtn = document.getElementById("removeBtn");
 
-      removeBtn.addEventListener("click", () => {
-        let selectedOption = colorSelect.value;
-        for (let i = 0; i < colorSelect.options.length; i++) {
-          if (colorSelect.options[i].value === selectedOption) {
-            colorSelect.remove(i);
-            break; 
-          }
-        }
-      });
+     removeBtn.addEventListener("click", () => {
+  let selectedIndex = colorSelect.selectedIndex;
+  if (selectedIndex !== -1) { 
+    colorSelect.remove(selectedIndex);
+  }
+});
